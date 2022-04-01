67192b52f582ae8217731e5b6666657e5ef5aa3d
Andree-Zzz <mad08061@gmail.com>
2022-03-31 22:42:04 -0500

    Cambiando db

--- database/db.sql
+++ database/db.sql
@@ -1,7 +1,7 @@
 -- --------------------------------------------------------
--- Host:                         academia.c1mebdhdxytu.us-east-1.rds.amazonaws.com
--- Versión del servidor:         10.4.21-MariaDB-log - Source distribution
--- SO del servidor:              Linux
+-- Host:                         127.0.0.1
+-- Versión del servidor:         5.7.33 - MySQL Community Server (GPL)
+-- SO del servidor:              Win64
 -- HeidiSQL Versión:             11.2.0.6213
 -- --------------------------------------------------------
 
@@ -13,20 +13,26 @@
 /*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
 
 
--- Volcando estructura de base de datos para p2
-CREATE DATABASE IF NOT EXISTS `p2` /*!40100 DEFAULT CHARACTER SET latin1 */;
-USE `p2`;
+-- Volcando estructura de base de datos para sistema_archivos
+CREATE DATABASE IF NOT EXISTS `sistema_archivos` /*!40100 DEFAULT CHARACTER SET utf8 */;
+USE `sistema_archivos`;
 
--- Volcando estructura para tabla p2.registro_user
-CREATE TABLE IF NOT EXISTS `registro_user` (
+-- Volcando estructura para tabla sistema_archivos.users
+CREATE TABLE IF NOT EXISTS `users` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `name` varchar(100) NOT NULL,
   `email` varchar(100) NOT NULL,
   `password` varchar(50) NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8;
 
--- La exportación de datos fue deseleccionada.
+-- Volcando datos para la tabla sistema_archivos.users: ~2 rows (aproximadamente)
+/*!40000 ALTER TABLE `users` DISABLE KEYS */;
+INSERT INTO `users` (`id`, `name`, `email`, `password`) VALUES
+	(1, 'Andrés Camilo Morales', 'perro@gg.com', '124234'),
+	(2, 'Andrés Camilo Morales', 'perro@gg.com', 'q'),
+	(3, 'Andrés Camilo Morales', 'perro@gg.com', '111');
+/*!40000 ALTER TABLE `users` ENABLE KEYS */;
 
 /*!40101 SET SQL_MODE=IFNULL(@OLD_SQL_MODE, '') */;
 /*!40014 SET FOREIGN_KEY_CHECKS=IFNULL(@OLD_FOREIGN_KEY_CHECKS, 1) */;
