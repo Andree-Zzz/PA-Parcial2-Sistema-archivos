acfc9803643271c7aa25357587b87ad33694cf1e
Andree-Zzz <mad08061@gmail.com>
2022-04-28 05:19:32 -0500

    Deploy a Heroku

--- main.py
+++ main.py
@@ -203,4 +203,4 @@ def status_404(error):
 app.register_error_handler(401,status_401)
 app.register_error_handler(404,status_404)
 csrf.init_app(app)
-app.run(debug=True)
+# app.run(debug=True) # Comentar para Heroku
